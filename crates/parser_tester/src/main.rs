d54e1ef71ea49d0e71a04ffd4f3258ad8bf4983a
David Richey <darichey1@gmail.com>
2022-07-10 21:09:07 -0500

    Revert "Change relative path handling (probably not right still)"
    This reverts commit bd6986f205deceefba983d1ade9b8d9023b57484.

--- crates/parser_tester/src/main.rs
+++ crates/parser_tester/src/main.rs
@@ -1,91 +1,33 @@
-// use std::{env, fs};
-
-// use ref_impl_parser::Parser as RefImplParser;
-// use rnix_to_json::Parser as RnixParser;
-
-// fn main() {
-//     let nix_exprs = [
-//         "x: { y = x.y; }",
-//         "x: { inherit (x) y; }",
-//     ];
-
-//     for expr in nix_exprs {
-//         println!("===========================================");
-
-//         println!("{expr}");
-
-//         println!("------------");
-
-//         let json_str1 = RefImplParser::new().parse(expr);
-//         println!("{json_str1}");
-
-//         println!("------------");
-
-//         let json_str2 = RnixParser::new(".", env::var("HOME").unwrap()).parse(expr);
-//         println!("{json_str2}");
-
-//         println!("------------");
-
-//         println!("{}", json_str1 == json_str2);
-//         println!("===========================================");
-//     }
-// }
+use std::env;
 
 use ref_impl_parser::Parser as RefImplParser;
 use rnix_to_json::Parser as RnixParser;
-use std::{env, error::Error, fs, path::Path};
 
-fn main() -> Result<(), Box<dyn Error>> {
-    let path = env::var("NIX_PATH")?;
-    let nixpkgs = path
-        .split(':')
-        .find(|s| s.starts_with("nixpkgs="))
-        .ok_or("no store path found")?;
+fn main() {
+    let nix_exprs = [
+        "x: ./foo/bar",
+        "x: foo/bar/bar",
+        "x: /foo/bar",
+        "x: ~/foo/bar",
+        "x: <foo/bar>",
+    ];
 
-    println!("Nix store path: {}", nixpkgs);
+    for expr in nix_exprs {
+        println!("===========================================");
 
-    recurse(Path::new(&nixpkgs["nixpkgs=".len()..]))
-}
-fn recurse(path: &Path) -> Result<(), Box<dyn Error>> {
-    if path.metadata()?.is_file() {
-        if path.extension().and_then(|s| s.to_str()) != Some("nix") {
-            return Ok(());
-        }
+        println!("{expr}");
 
-        println!("Checking {}", path.display());
-        let nix_expr = fs::read_to_string(path)?;
-        if nix_expr.trim().is_empty() {
-            return Ok(());
-        }
+        println!("------------");
 
-        let json_str1 = RefImplParser::new().parse(&nix_expr);
-        let json_str2 = RnixParser::new(".", env::var("HOME").unwrap()).parse(&nix_expr);
+        let json_str1 = RefImplParser::new().parse(expr);
+        println!("{json_str1}");
 
-        if json_str1 != json_str2 {
-            println!("Parses not equal!");
-            println!("Input:");
-            println!("----------");
-            println!("{}", nix_expr);
-            println!("----------");
-            println!("Reference Impl Parse:");
-            println!("----------");
-            println!("{}", json_str1);
-            println!("----------");
-            println!("rnix-parser Parse:");
-            println!("----------");
-            println!("{}", json_str2);
-            return Err("Parses not equal".into());
-        }
+        let json_str2 = RnixParser::new(".", env::var("HOME").unwrap()).parse(expr);
+        println!("{json_str2}");
 
-        return Ok(());
-    } else {
-        for entry in path.read_dir()? {
-            let entry = entry?;
-            if entry.file_type()?.is_symlink() {
-                continue;
-            }
-            recurse(&entry.path())?;
-        }
+        println!("------------");
+
+        println!("{}", json_str1 == json_str2);
+        println!("===========================================");
     }
-    Ok(())
 }
