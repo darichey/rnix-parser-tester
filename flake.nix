31fba462c680e5ebeacf75a793edc69c8cde9b71
David Richey <darichey1@gmail.com>
2022-07-19 00:03:56 -0500

    Patch Nix to combine consecutive StringTokens

--- flake.nix
+++ flake.nix
@@ -2,7 +2,18 @@
   inputs.nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
   outputs = { self, nixpkgs }:
     let
-      pkgs = nixpkgs.legacyPackages.x86_64-linux;
+      pkgs = import nixpkgs {
+        system = "x86_64-linux";
+        overlays = [
+          (final: prev: {
+            nix = prev.nix.overrideAttrs (old: {
+              patches = (old.patches or [ ]) ++ [
+                ./crates/ref_impl_parser/reference_to_json/patch/combine-string-token.patch
+              ];
+            });
+          })
+        ];
+      };
     in
     {
       devShells.x86_64-linux.default = pkgs.mkShell {
